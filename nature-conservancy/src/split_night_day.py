dceb938585eb9f14e09c8d7b3fdcc5ffb1206d73
JulesSauvinet <julessauvinet@yahoo.fr>
2017-02-15 23:18:37 +0100

    ajout du code de boat clustering (to use later)

--- nature-conservancy/src/split_night_day.py
+++ nature-conservancy/src/split_night_day.py
@@ -1,3 +1,5 @@
+# https://www.kaggle.com/qiubit/the-nature-conservancy-fisheries-monitoring/detecting-night-photos
+
 import numpy as np
 import glob
 import random
@@ -9,6 +11,65 @@ from sklearn import cluster
 from sklearn import neighbors
 
 from scipy.misc import imread, imsave
+
+# load images
+imgs_to_load = 20
+
+preview_files = sorted(glob.glob('../input/train/*/*.jpg'), key=lambda x: random.random())[:imgs_to_load]
+preview = np.array([imread(img) for img in preview_files])
+
+def show_loaded_with_means(imgs):
+    rows_total = int(len(preview) / 4)
+    for i in range(rows_total):
+        _, img_ax = plt.subplots(1, 4, sharex='col', sharey='row', figsize=(8, 2))
+        _, imgmean_ax = plt.subplots(1, 4, sharex='col', sharey='row', figsize=(8, 2))
+        for j in range(4):
+            img = preview[i * 4 + j]
+            img_mean = np.mean(img, axis=(0, 1))
+            # calculate squared means to amplify green dominance effect
+            img_mean = np.power(img_mean, 2)
+
+            # show plots
+            img_ax[j].axis('off')
+            img_ax[j].imshow(img)
+            imgmean_ax[j].bar(range(3), img_mean, width=0.3, color='blue')
+            imgmean_ax[j].set_xticks(np.arange(3) + 0.3 / 2)
+            imgmean_ax[j].set_xticklabels(['R', 'G', 'B'])
+
+
+show_loaded_with_means(preview)
+plt.show()
+
+imgs_to_load = 20
+
+preview_files = sorted(glob.glob('../input/train/*/*.jpg'), key=lambda x: random.random())[:imgs_to_load]
+preview = np.array([imread(img) for img in preview_files])
+
+
+def show_loaded_with_mean_differences(imgs):
+    rows_total = int(len(preview) / 4)
+    for i in range(rows_total):
+        _, img_ax = plt.subplots(1, 4, sharex='col', sharey='row', figsize=(8, 2))
+        _, imgmean_ax = plt.subplots(1, 4, sharex='col', sharey='row', figsize=(8, 2))
+        for j in range(4):
+            # calculate features of an image
+            img = preview[i * 4 + j]
+            img_mean = np.mean(img, axis=(0, 1))
+            img_features = np.zeros(3)
+            img_features[0] = (img_mean[0] - img_mean[1]) + (img_mean[0] - img_mean[2])
+            img_features[1] = (img_mean[1] - img_mean[0]) + (img_mean[1] - img_mean[2])
+            img_features[2] = (img_mean[2] - img_mean[0]) + (img_mean[2] - img_mean[1])
+
+            # display plots
+            img_ax[j].axis('off')
+            img_ax[j].imshow(img)
+            imgmean_ax[j].bar(range(3), img_features, width=0.3, color='blue')
+            imgmean_ax[j].set_xticks(np.arange(3) + 0.3 / 2)
+            imgmean_ax[j].set_xticklabels(['R', 'G', 'B'])
+
+
+show_loaded_with_mean_differences(preview)
+plt.show()
 
 # one cluster will be day photos, the other one night photos
 knn_cls = 2
@@ -30,11 +91,22 @@ for i in range(training_imgs):
 kmeans = cluster.KMeans(n_clusters=knn_cls).fit(training_features)
 print(np.bincount(kmeans.labels_))
 
+def show_four(imgs, title):
+    _, ax = plt.subplots(1, 4, sharex='col', sharey='row', figsize=(8, 2))
+    plt.suptitle(title, size=8)
+    for i, img in enumerate(imgs[:4]):
+        ax[i].axis('off')
+        ax[i].imshow(img)
+
+for i in range(knn_cls):
+    cluster_i = training[np.where(kmeans.labels_ == i)]
+    show_four(cluster_i[:4], 'cluster' + str(i))
+
 batch = 100
 
 # now load all training examples and cluster them
-CLUSTER_FOLDER = os.path.abspath('./data/train/clustered')
-training_filenames = sorted(glob.glob('./data/train/*/*.jpg'))
+CLUSTER_FOLDER = os.path.abspath('../input/train/clustered')
+training_filenames = sorted(glob.glob('../input/train/*/*.jpg'))
 
 # make directories if they doesn't exist
 if not os.path.isdir(CLUSTER_FOLDER):
@@ -45,6 +117,7 @@ for cluster_num in xrange(knn_cls):
     if not os.path.isdir(single_cluster_folder):
         os.mkdir(single_cluster_folder)
 
+print (training_filenames)
 saved_files = 0
 while saved_files < len(training_filenames):
     training_files = training_filenames[saved_files:saved_files+batch]
