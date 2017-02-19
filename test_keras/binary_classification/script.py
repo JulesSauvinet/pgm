9ec49b4e48178e56088a641f84fa017a467f83be
Marine RUIZ <Marine RUIZ>
2017-02-19 14:38:00 +0100

    Ajout test Keras 2

--- test_keras/binary_classification/script.py
+++ test_keras/binary_classification/script.py
@@ -29,6 +29,29 @@ def create_baseline():
     return model
 
 
+# smaller model
+def create_smaller():
+    # create model
+    model = Sequential()
+    model.add(Dense(30, input_dim=60, init='normal', activation='relu'))
+    model.add(Dense(1, init='normal', activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+# larger model
+def create_larger():
+	# create model
+	model = Sequential()
+	model.add(Dense(60, input_dim=60, init='normal', activation='relu'))
+	model.add(Dense(30, init='normal', activation='relu'))
+	model.add(Dense(1, init='normal', activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+    
 
 # fix random seed for reproducibility
 seed = 5
@@ -52,9 +75,44 @@ encoded_Y = encoder.transform(Y)
 print('...done\n')
 
 # evaluate model with standardized dataset
-print('Creation du model')
+print('Creation du model avec standardized dataset')
 estimator = KerasClassifier(build_fn=create_baseline, nb_epoch=100, batch_size=5, verbose=0)
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
 results = cross_val_score(estimator, X, encoded_Y, cv=kfold)
 print('...done\n')
 print("Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+
+# evaluate baseline model with standardized dataset
+print('Creation du baseline model avec standardized dataset')
+numpy.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasClassifier(build_fn=create_baseline, nb_epoch=100, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(pipeline, X, encoded_Y, cv=kfold)
+print("Standardized: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+
+# evaluate model with smaller dataset
+print('\nCreation du model avec smaller dataset')
+numpy.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasClassifier(build_fn=create_smaller, nb_epoch=100, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(pipeline, X, encoded_Y, cv=kfold)
+print("Smaller: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+# evaluate model with larger dataset
+print('\nCreation du model avec larger dataset')
+numpy.random.seed(seed)
+estimators = []
+estimators.append(('standardize', StandardScaler()))
+estimators.append(('mlp', KerasClassifier(build_fn=create_larger, nb_epoch=100, batch_size=5, verbose=0)))
+pipeline = Pipeline(estimators)
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(pipeline, X, encoded_Y, cv=kfold)
+print("Larger: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
